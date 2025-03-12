5835ab26bf0b228b560325aa60b90b65e74161e5
HornetRzn <hornetrzn@gmail.com>
2025-03-12 19:18:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,16 @@ import os
 import logging
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 from telegram.ext import (
-    Updater,
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+Updater,
     CommandHandler,
     CallbackQueryHandler,
     MessageHandler,
